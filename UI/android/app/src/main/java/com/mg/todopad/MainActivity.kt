d1c578ea914aec47d6e81b6b8010680bb3e00534
Madhan Ganesh L <madhanganesh@Madhans-Mac-mini.local>
2025-02-15 19:17:45 +0530

    added Android src files and privacy policy

---
+++ UI/android/app/src/main/java/com/mg/todopad/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.mg.todopad
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+
+        //val url = if (BuildConfig.DEBUG) "http://localhost:8080" else "https://todopad.in"
+        val url = "https://todopad.in"
+        webView.loadUrl(url)
+
+        setContentView(webView)
+    }
+}
